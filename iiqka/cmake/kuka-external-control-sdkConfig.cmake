8b0c79da4e6c76da7d4e0055a03735b1f81a1505
Svastits <49677296+Svastits@users.noreply.github.com>
2025-08-26 14:10:00 +0200

    Rebase from 'release/humble/kuka_external_control_sdk'

--- iiqka/cmake/kuka-external-control-sdkConfig.cmake
+++
@@ -1,7 +0,0 @@
-include(CMakeFindDependencyMacro)
-
-
-find_dependency(OpenSSL REQUIRED)
-find_dependency(Threads REQUIRED)
-
-include("${CMAKE_CURRENT_LIST_DIR}/kuka-external-control-sdkTargets.cmake")
